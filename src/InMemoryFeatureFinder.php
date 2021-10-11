48dec0fe14f2725cd6c1abb5045c30d508cc04c5
Koldo Picaza <1093654+kpicaza@users.noreply.github.com>
2021-10-11 14:14:37 +0200

    [#6] return numeric keyed array from feature finder

--- src/InMemoryFeatureFinder.php
+++ src/InMemoryFeatureFinder.php
@@ -7,6 +7,9 @@ namespace Pheature\InMemory\Toggle;
 use Pheature\Core\Toggle\Read\Feature;
 use Pheature\Core\Toggle\Read\FeatureFinder;
 
+use function array_map;
+use function array_values;
+
 final class InMemoryFeatureFinder implements FeatureFinder
 {
     private InMemoryConfig $config;
@@ -29,10 +32,12 @@ final class InMemoryFeatureFinder implements FeatureFinder
 
     public function all(): array
     {
-        return array_map(
-            /** @param array<string, string|bool|array<string, mixed>> $feature */
-            fn(array $feature) => $this->featureFactory->create($feature),
-            $this->config->all()
+        return array_values(
+            array_map(
+                /** @param array<string, string|bool|array<string, mixed>> $feature */
+                fn(array $feature): Feature => $this->featureFactory->create($feature),
+                $this->config->all()
+            )
         );
     }
 }
